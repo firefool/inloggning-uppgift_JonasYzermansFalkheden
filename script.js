e279ffab377aece2ef962858c87af0956ce2f3a1
Jonas Yzermans <jonazyzermans@gmail.com>
2020-09-09 00:32:31 +0200

    Feature complete

--- script.js
+++ script.js
@@ -14,6 +14,13 @@ function login(userName, passWord)
     var userExists = false;
     var userID;
 
+    // Om något fält är tomt så skrivs ett felmeddelande ut
+    if(userName == "" || passWord == "")
+    {
+        document.getElementById("loginMessage").innerText = "Du glömde något!";
+        return;
+    }
+
     // Undersöker först om det finns en användare med namnet som angivits
     for(i = 0; i < users.length; i++)
     {
@@ -30,36 +37,105 @@ function login(userName, passWord)
     {
         if(users[userID].pass == passWord)
         {
-            console.log("Success");
+            loggedIn = true;           
+            lStorage.setItem("Username", "test");
+            lStorage.setItem("LoggedIn", "True");
+            switchDisplay("userDisplay");
         }
         else
         {
-            console.log("fail");
+            switchDisplay("failDisplay");
         }
     }
     else
     {
-        console.log("no user detected");
+        switchDisplay("failDisplay");
+    }
+}
+
+// Funktion för att byta vad som ska visas upp på skärmen, tar emot tre olika sorters "displayer"
+function switchDisplay(displayName)
+{
+    var loginDiv = document.getElementById("loginDisplay");
+    var userDiv = document.getElementById("userDisplay");
+    var failDiv = document.getElementById("loginFailedDisplay");
+    document.getElementById("loginMessage").innerText = "";
+    document.getElementById("username").value = "";  
+    document.getElementById("password").value = "";  
+
+    if(displayName == "loginDisplay")
+    {
+        userDiv.style.display = "none";
+        loginDiv.style.display = "block";
+        failDiv.style.display = "none";
+    }
+    if(displayName == "userDisplay")
+    {
+        userDiv.style.display = "block";
+        loginDiv.style.display = "none";
+        failDiv.style.display = "none"; 
+        document.getElementById("welcomeText").innerText = "Välkommen in, " + lStorage.getItem("Username") + "!";   
+    }
+    if(displayName == "failDisplay")
+    {
+        userDiv.style.display = "none";
+        loginDiv.style.display = "none";
+        failDiv.style.display = "block";
     }
 }
 
 // Skapar våran testanvändare
 var testUser = new User("test", "1234");
-
 // Skapar en array med våran testanvändare, med en array kan vi lättare lägga till flera användare i framtiden
 let users = [testUser]
+var loggedIn = false;
+var currentUser = "";
+var lStorage = window.localStorage;
+
+if(lStorage.getItem("LoggedIn") == "True")
+{
+    loggedIn = true;
+    currentUser = lStorage.getItem("Username");
+}
+else
+{
+    currentUser = "";
+    loggedIn = false;
+}
+
+if (loggedIn)
+{
+    switchDisplay("userDisplay")
+}
+else
+{
+    switchDisplay("loginDisplay")
+}
 
 var loginButton = document.getElementById("login");
-
 loginButton.addEventListener("click", function buttonClick()
 {
     var userName_ = document.getElementById("username").value;
     var passWord_ = document.getElementById("password").value;
-    console.log("Tried to log in");
-    console.log(userName_);
-    console.log(passWord_);
     login(userName_, passWord_);
 });
+
+var logoutButton = document.getElementById("logout");
+logoutButton.addEventListener("click", function logoutClick()
+{
+    loggedIn = false;
+    lStorage.clear();
+    switchDisplay("loginDisplay");
+});
+
+var tryAgainButton = document.getElementById("tryagain");
+tryAgainButton.addEventListener("click", function tryAgainClick()
+{
+    switchDisplay("loginDisplay");
+});
+
+
+
 
 
 
