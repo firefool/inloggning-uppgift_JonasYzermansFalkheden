e15c07ea395b9b6da0c28e9ad4a4cc0744446565
Jonas Yzermans <jonazyzermans@gmail.com>
2020-09-09 00:58:31 +0200

    Rensat kod och kommenterat

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-// Klass för användare
+// Simpel användarklass med namn och lösenord
 class User{
     constructor(name, pass)
     {
@@ -21,7 +21,7 @@ function login(userName, passWord)
         return;
     }
 
-    // Undersöker först om det finns en användare med namnet som angivits
+    // Undersöker först om det finns en användare med namnet som angivits genom att loopa igenom listan av användare
     for(i = 0; i < users.length; i++)
     {
         if(users[i].name == userName)
@@ -35,18 +35,19 @@ function login(userName, passWord)
     // Om en användare hittas så kollar vi om lösenordet stämmer
     if(userExists)
     {
+        // Om lösenordet stämmer så visar vi våran inloggad-display och sparar användarnamnet i localStorage
         if(users[userID].pass == passWord)
         {
-            loggedIn = true;           
             lStorage.setItem("Username", "test");
-            lStorage.setItem("LoggedIn", "True");
             switchDisplay("userDisplay");
         }
+        // Annars visar vi våran försökigen-display
         else
         {
             switchDisplay("failDisplay");
         }
     }
+    // Om användaren inte hittas visas också våran försökigen-display
     else
     {
         switchDisplay("failDisplay");
@@ -56,13 +57,17 @@ function login(userName, passWord)
 // Funktion för att byta vad som ska visas upp på skärmen, tar emot tre olika sorters "displayer"
 function switchDisplay(displayName)
 {
+    // Här hämtas våra tre olika display-divs 
     var loginDiv = document.getElementById("loginDisplay");
     var userDiv = document.getElementById("userDisplay");
     var failDiv = document.getElementById("loginFailedDisplay");
+
+    // Här återställs alla textfält så att allt ser snyggt ut när vi byter display
     document.getElementById("loginMessage").innerText = "";
     document.getElementById("username").value = "";  
     document.getElementById("password").value = "";  
 
+    // Baserat på vilken input funktionen fått så döjs och visas de relevanta sektionerna.
     if(displayName == "loginDisplay")
     {
         userDiv.style.display = "none";
@@ -84,55 +89,55 @@ function switchDisplay(displayName)
     }
 }
 
-// Skapar våran testanvändare
+// Håller reda på alla våra knappar och vad som ska hända när dessa klickas
+function buttons()
+{
+    // Loginknappen tar in våran input från textfälten och skickar vidare detta till våran login funktion.
+    var loginButton = document.getElementById("login");
+    loginButton.addEventListener("click", function buttonClick()
+    {
+        var userName_ = document.getElementById("username").value;
+        var passWord_ = document.getElementById("password").value;
+        login(userName_, passWord_);
+    });
+
+    // Logoutknappen, tömmer våran localStorage och byter till inloggningssidan igen
+    var logoutButton = document.getElementById("logout");
+    logoutButton.addEventListener("click", function logoutClick()
+    {
+        lStorage.clear();
+        switchDisplay("loginDisplay");
+    });
+
+    // Försökigenknappen tar dig tillbaks till inloggningssidan
+    var tryAgainButton = document.getElementById("tryagain");
+    tryAgainButton.addEventListener("click", function tryAgainClick()
+    {
+        switchDisplay("loginDisplay");
+    });
+}
+
+
+// Skapar våran testanvändare och lägger in denna i en array. Använder en array så vi kan lägga till flera användare i framtiden.
 var testUser = new User("test", "1234");
-// Skapar en array med våran testanvändare, med en array kan vi lättare lägga till flera användare i framtiden
 let users = [testUser]
-var loggedIn = false;
-var currentUser = "";
+
+// Hämtar våran localStorage och kollar om det finns någon inloggad, visar sedan upp rätt display baserat på om du är inloggad eller inte. 
 var lStorage = window.localStorage;
-
-if(lStorage.getItem("LoggedIn") == "True")
-{
-    loggedIn = true;
-    currentUser = lStorage.getItem("Username");
-}
-else
-{
-    currentUser = "";
-    loggedIn = false;
-}
-
-if (loggedIn)
+if(lStorage.getItem("Username"))
 {
     switchDisplay("userDisplay")
+
 }
 else
 {
     switchDisplay("loginDisplay")
+
 }
 
-var loginButton = document.getElementById("login");
-loginButton.addEventListener("click", function buttonClick()
-{
-    var userName_ = document.getElementById("username").value;
-    var passWord_ = document.getElementById("password").value;
-    login(userName_, passWord_);
-});
+//Kör våran buttons funktion
+buttons();
 
-var logoutButton = document.getElementById("logout");
-logoutButton.addEventListener("click", function logoutClick()
-{
-    loggedIn = false;
-    lStorage.clear();
-    switchDisplay("loginDisplay");
-});
-
-var tryAgainButton = document.getElementById("tryagain");
-tryAgainButton.addEventListener("click", function tryAgainClick()
-{
-    switchDisplay("loginDisplay");
-});
 
 
 
